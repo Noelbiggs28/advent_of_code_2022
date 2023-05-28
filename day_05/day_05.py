2424bdd8a9ee0a19fd2a7db5654bbb81c818bb15
noelbiggs28@gmail.com <noelbiggs28@gmail.com>
2023-05-27 16:01:52 -1000

    attempted day 4

---
+++ day_05/day_05.py
@@ -0,0 +1,25 @@
+def part1(instructions):
+    ls=['zero',
+     'FCPGQR'
+    , 'WTCP'
+    , 'BHPMC'
+    , 'LTQSMPR'
+    , 'PHJZVGN'
+    , 'DPJ'
+    , 'LGPZFJTR'
+    , 'NLHCFPTJ'
+    , 'GVZQHTCW'
+    ]
+    with open(instructions, 'r')  as infile:
+        for line in infile:
+            line = line.strip()
+            arr = line.split(' ')
+            move = int(arr[1])
+            begin = int(arr[3])
+            to = int(arr[5])
+            crate= ls[begin][-move:]
+            ls[begin] = ls[begin][:-move]
+            ls[to] = ls[to] + crate
+    return ls
+# WJVRLSJJT
+print(part1('instructions.txt'))
