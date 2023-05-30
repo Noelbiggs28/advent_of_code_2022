8d41b547511904a2c9289e155a0223ff5d73e920
noel_biggs <noelbiggs28@gmail.com>
2023-05-30 10:00:55 -1000

    made day 02 more readable

--- day_02/day_02.py
+++ day_02/day_02.py
@@ -1,17 +1,13 @@
 def shoot():
     total_score = 0
-    # base_score = {
-    #     'X':'1',
-    #     'Y':'2',
-    #     'Z':'3'
-    # }
     base_score = {
-        'X':'0',
-        'Y':'3',
-        'Z':'6'
+        'X':'1',
+        'Y':'2',
+        'Z':'3'
     }
-    # winning_score = {'A':{'X':'3','Y':'6','Z':'0'},'B':{'X':'0','Y':'3','Z':'6'},'C':{'X':'6','Y':'0','Z':'3'}}
-    winning_score = {'A':{'X':'3','Y':'1','Z':'2'},'B':{'X':'1','Y':'2','Z':'3'},'C':{'X':'2','Y':'3','Z':'1'}}
+    winning_score = {'A':{'X':'3','Y':'6','Z':'0'},
+                     'B':{'X':'0','Y':'3','Z':'6'},
+                     'C':{'X':'6','Y':'0','Z':'3'}}
     with open('strategy.txt', 'r') as infile:
         for line in infile:
             opponent = line[0].strip()
@@ -19,4 +15,23 @@ def shoot():
             total_score += int(base_score[me]) 
             total_score += int(winning_score[opponent][me])
     return total_score 
+
+def shoot2():
+    total_score = 0
+    winning_score = {
+    'X':'0',
+    'Y':'3',
+    'Z':'6'
+    }
+    base_score = {'A':{'X':'3','Y':'1','Z':'2'},
+                  'B':{'X':'1','Y':'2','Z':'3'},
+                  'C':{'X':'2','Y':'3','Z':'1'}}
+    with open('strategy.txt', 'r') as infile:
+        for line in infile:
+            opponent = line[0].strip()
+            me =line[2].strip()
+            total_score += int(winning_score[me]) 
+            total_score += int(base_score[opponent][me])
+    return total_score 
 print(shoot())
+print(shoot2())
